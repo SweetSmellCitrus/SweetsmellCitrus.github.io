685f393d7e66f408aa4042efb3690546077871d7
SweetSmellCitrus <63629362+SweetSmellCitrus@users.noreply.github.com>
2020-04-21 10:33:33 +0800

    Add files via upload

---
+++ demo01.js
@@ -0,0 +1,27 @@
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+function per(){
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function() {
+		 showPic(this);
+         return false;
+		
+        }
+    }
+}
+function showPic(whicPic) {
+        var source = whicPic.getAttribute("href")
+        var flag = document.getElementById("flag");
+        flag.setAttribute("src", source);
+    }
+addLoadEvent(per);
